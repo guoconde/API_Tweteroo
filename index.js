678a3acd79664158bc99bc58e9bc0ad8fcb0d200
Gustavo Conde <guoconde@pop-os.localdomain>
2022-01-21 12:14:08 -0300

    Finalizando projeto e aguardando liberacao dos bonus

--- index.js
+++ index.js
@@ -8,13 +8,33 @@ app.use(json())
 
 let user
 
-app.get('/', (_, res) => {
-    res.send('rodou')
+const tweets = [
+    {
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+        tweet: "eu amo o hub"
+    }
+]
+
+app.get('/tweets', (_, res) => {
+    res.send(tweets)
 })
 
 app.post('/sign-up', (req, res) => {
     user = req.body
-    console.log(user)
+
+    res.send('ok')
+})
+
+app.post('/tweets', (req, res) => {
+    const tweet = req.body
+    tweet.avatar = user.avatar
+
+    if(tweets.length === 10) {
+        tweets.shift()
+    }
+
+    tweets.push(tweet)
 
     res.send('ok')
 })
