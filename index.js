5d57f0b8d2eb5cc165439aca6b813cdcbf690140
Gustavo Conde <guoconde@pop-os.localdomain>
2022-01-21 11:53:36 -0300

    Implementando post sign-up

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('ainda esta rodando')
+import express, { json } from "express";
+import cors from 'cors';
+
+const app = express()
+
+app.use(cors())
+app.use(json())
+
+let user
+
+app.get('/', (_, res) => {
+    res.send('rodou')
+})
+
+app.post('/sign-up', (req, res) => {
+    user = req.body
+    console.log(user)
+
+    res.send('ok')
+})
+
+app.listen(5000)
